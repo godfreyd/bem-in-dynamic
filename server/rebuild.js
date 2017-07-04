65851ffdfdd968624406dcf327da6fb4c68cbeba
Sergei Bocharov <sergei-b84@bk.ru>
2017-07-04 15:45:18 +0300

    rebuild

--- server/rebuild.js
+++ server/rebuild.js
@@ -1,4 +1,6 @@
-var path = require('path'),
+var fs = require('fs'),
+    path = require('path'),
+    _ = require('lodash'),
     tinyLr = require('tiny-lr'),
     notifier = require('node-notifier'),
     make = require('enb').make,
@@ -10,34 +12,50 @@ var path = require('path'),
         ignoreInitial: true
     };
 
+// get bundles list
+var bundlesDir = path.join(rootDir, 'desktop.bundles');
+var bundles = fs.readdirSync(bundlesDir).filter(function(file) {
+    return fs.statSync(path.join(bundlesDir, file)).isDirectory();
+});
+
 // enb make
-process.env.NO_AUTOMAKE || watch(path.join(rootDir, '*.blocks/**'), watchOpts)
-    .on('all', function(event, file) {
-        console.time('Rebuild: ' + file);
-        process.nextTick(function() {
-            make()
-                .then(function() {
-                    console.timeEnd('Rebuild: ' + file);
-                    notifier.notify({
-                        title: 'Tycoon',
-                        message: 'Build finished'
-                    });
-                })
-                .fail(function(err) {
-                    notifier.notify({
-                        title: 'Build failed',
-                        message: err
-                    });
-                });
+function rebuild(event, file) {
+    // TODO: get target via file extention
+    // TODO: get current bundle via websocket
+    // NOTE: use `[path.join('desktop.bundles', 'index')]` to build specific target
+
+    console.time('Rebuild: ' + file);
+    return make()
+        .then(function() {
+            console.timeEnd('Rebuild: ' + file);
+            notifier.notify({
+                title: 'bem-express',
+                message: 'Build finished'
+            });
+        })
+        .fail(function(err) {
+            notifier.notify({
+                title: 'Build failed',
+                message: err
+            });
         });
-    });
+}
+
+var debouncedRebuild = _.debounce(rebuild, 30, { leading: true, trailing: true });
+
+process.env.NO_AUTOMAKE || watch([
+    path.join(rootDir, '*.blocks', '**'),
+].concat(bundles.map(function(bundle) {
+    return path.join(bundlesDir, bundle, bundle + '.bemdecl.js');
+})), watchOpts).on('all', debouncedRebuild);
 
 // livereload
 process.env.NO_LIVERELOAD || watch([
     path.join(rootDir, 'static', '*.min.*'),
-    path.join(rootDir, 'desktop.bundles/*/*.bemhtml.js'),
-    path.join(rootDir, 'desktop.bundles/*/*.bemtree.js'),
-], watchOpts).on('all', function(event, file) {
+    path.join(bundlesDir, '*', '*.bemtree.js'),
+].concat(bundles.map(function(bundle) {
+    return path.join(bundlesDir, bundle, bundle + '.bemhtml.js');
+})), watchOpts).on('all', function(event, file) {
     tinyLr.changed(file);
 });
 
